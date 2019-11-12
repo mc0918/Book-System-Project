d2cef52c1e3e19d4224c9d6bcd8b7d0564df34b1
Michael Campbell <mcampbell0918@gmail.com>
2019-11-12 09:59:40 -0600

    added feign client annotation to queue

--- note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/NoteQueueConsumerApplication.java
+++ note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/NoteQueueConsumerApplication.java
@@ -7,9 +7,11 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NoteQueueConsumerApplication {
 
 
