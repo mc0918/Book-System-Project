17e6590fc823d1ef4dd6e18a43b1ae8439649903
Michael Campbell <mcampbell0918@gmail.com>
2019-11-12 09:28:03 -0600

    eureka server

---
+++ book-system-eureka-server/src/test/java/com/trilogyed/booksystemeurekaserver/BookSystemEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.booksystemeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSystemEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
