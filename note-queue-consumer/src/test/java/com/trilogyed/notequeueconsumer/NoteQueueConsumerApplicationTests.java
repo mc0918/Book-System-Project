ecaa92cbb5b789108b9a4e53496dc22b341ec081
Michael Campbell <mcampbell0918@gmail.com>
2019-11-12 09:36:41 -0600

    book service, note service, note queue

---
+++ note-queue-consumer/src/test/java/com/trilogyed/notequeueconsumer/NoteQueueConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.notequeueconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteQueueConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
