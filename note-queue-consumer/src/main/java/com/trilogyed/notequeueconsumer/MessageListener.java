e0e0cd3e9316b5889da160c191fc7fe3b022c1cd
Michael Campbell <mcampbell0918@gmail.com>
2019-11-13 10:12:00 -0600

    queue logic

--- note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/MessageListener.java
+++ note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/MessageListener.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 public class MessageListener {
@@ -21,8 +22,29 @@ public class MessageListener {
     @RabbitListener(queues = NoteQueueConsumerApplication.QUEUE_NAME)
     public void receiveMessage(NoteEntry msg) {
         System.out.println(msg.toString());
-        //Todo: create or update note
-        noteClient.saveNoteToDb(msg); //will this work???
-        System.out.println("But, like, basically, I am " + msg.toString());
+
+        boolean isNewNote = true;
+
+        List<NoteEntry> notes = noteClient.getAllNotes();
+        for (NoteEntry note : notes) {
+            if (msg.getNote_id() == note.getNote_id()) {
+                isNewNote = false;
+                break;
+            }
+        }
+
+        if (isNewNote) {
+            noteClient.saveNoteToDb(msg);
+            System.out.println(msg + " saved to database!");
+        } else {
+            noteClient.updateNoteinDb(msg);
+            System.out.println("note with id " + msg.getNote_id() + " updated");
+            isNewNote = true;
+        }
     }
+
+
+//        noteClient.saveNoteToDb(msg); //will this work???
+//        System.out.println("But, like, basically, I am " + msg.toString());
+
 }
--- note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/util/feign/NoteClient.java
+++ note-queue-consumer/src/main/java/com/trilogyed/notequeueconsumer/util/feign/NoteClient.java
@@ -2,6 +2,7 @@ package com.trilogyed.notequeueconsumer.util.feign;
 
 import com.trilogyed.notequeueconsumer.util.messages.NoteEntry;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +12,9 @@ import java.util.List;
 
 @FeignClient(name = "note-service")
 public interface NoteClient {
-//    @RequestMapping(value = "/notes/book/{book_id}", method = RequestMethod.GET)
-//    public List<NoteEntry> getNotesByBook(@PathVariable int book_id);
+
+    @GetMapping("/notes")
+    public List<NoteEntry> getAllNotes();
 
     @RequestMapping(value = "/notes", method = RequestMethod.POST)
     public NoteEntry saveNoteToDb(NoteEntry noteEntry);
